ffd8f8a4a88f4a38488f7f6bd75ec37aff9f8b2e
Nicolas Jorand <nicolas.jorand@me.com>
2017-12-02 16:27:47 +0100

    Add the ui intercation

--- mate/Assets/Scripts/WebsocketServer.cs
+++ mate/Assets/Scripts/WebsocketServer.cs
@@ -3,39 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using System.Globalization;
+
+public class WebsocketServer : MonoBehaviour
+{
+
+    public GameObject pingu;
+
+    public const float lowerbound = 4.8f;
+    public const float upperbound = 5.2f;
+
+    private WebSocketServer wssv;
+
+    public static float weight = 0;
 
 
-public class WebsocketServer : MonoBehaviour {
-
-	public  GameObject pingu;
-   
-
-	// Use this for initialization
-	void Start () {
-        WSWorker.pingu = pingu;
+    // Use this for initialization
+    void Start()
+    {
         Debug.Log("Starting websocket server");
-        var wssv = new WebSocketServer("ws://0.0.0.0:8086");
+        wssv = new WebSocketServer("ws://0.0.0.0:8086");
         wssv.AddWebSocketService<WSWorker>("/");
         wssv.Start();
         Debug.Log("WebSocket started !");
     }
-    
+
     // Update is called once per frame
-    void Update () {
-        
+    void Update()
+    {
+        grow();
+    }
+
+    void OnDestroy()
+    {
+        wssv.Stop();
+    }
+
+    void grow()
+    {
+        var width = pingu.GetComponent<Renderer>().bounds.size.x;
+        if (weight > 10)
+        {
+            pingu.transform.localScale = new Vector3(weight, weight, weight);
+        }
+        else if (width > lowerbound && width < upperbound)
+        {
+            Debug.Log("GOT IT");
+        }
+        else if (weight < lowerbound)
+        {
+            pingu.transform.localScale = new Vector3(weight, weight, weight);
+            pingu.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else if (weight > upperbound)
+        {
+            pingu.transform.localScale = new Vector3(weight, weight, weight);
+            pingu.GetComponent<Renderer>().material.color = Color.red;
+        }
+        else
+        {
+            pingu.transform.localScale = new Vector3(weight, weight, weight);
+            pingu.GetComponent<Renderer>().material.color = Color.green;
+        }
     }
 }
 
 public class WSWorker : WebSocketBehavior
 {
-	static public GameObject pingu;
-	protected override void OnMessage(MessageEventArgs e)
-
+    protected override void OnMessage(MessageEventArgs e)
     {
         Debug.Log("Got message");
         Debug.Log(e.Data);
-        var weight = Mathf.Abs(float.Parse(e.Data));
-		var factor = weight / 200;
-       pingu.transform.localScale = new Vector3(factor, factor, factor);
+        var weight = Mathf.Abs(float.Parse(e.Data, CultureInfo.InvariantCulture.NumberFormat));
+        var factor = weight / 200;
+        WebsocketServer.weight = weight;
     }
+
+
 }
