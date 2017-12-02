2cfabaf00d1eb45b3f81da7af4b46a53d33140e6
Nicolas Jorand <nicolas.jorand@me.com>
2017-12-02 12:00:00 +0100

    add the socket cnnection

---
+++ mate/Assets/Scripts/WebsocketServer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+public class WebsocketServer : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Starting websocket server");
+        var wssv = new WebSocketServer("ws://0.0.0.0:8086");
+        wssv.AddWebSocketService<WSWorker>("/");
+        wssv.Start();
+        Debug.Log("WebSocket started !");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class WSWorker : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        Debug.Log("Got message");
+        var msg = e.Data == "BALUS"
+                  ? "I've been balused already..."
+                  : "I'm not available now.";
+        Debug.Log(e.Data);
+        Send(msg);
+    }
+}
